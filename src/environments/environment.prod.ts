5893ff08dae32da45011c5b9324f21b2784cc50f
Joy <santunu23@gmail.com>
2022-07-16 09:21:51 +0600

    second commit with initial settings.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+            apiKey: "AIzaSyBq7CiO2__yeOZ3vONXJAXD9fa0MG0vzRk",
+            authDomain: "bita-reunion.firebaseapp.com",
+            projectId: "bita-reunion",
+            storageBucket: "bita-reunion.appspot.com",
+            messagingSenderId: "143404432811",
+            appId: "1:143404432811:web:e1e77d591ced101077a6ee",
+            measurementId: "G-55VRP38B6H"
+  }
 };

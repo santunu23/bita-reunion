17b76dc4719bb275d7504bd661e46ce3d61daf99
Joy <santunu23@gmail.com>
2022-07-28 07:27:31 +0600

    =Added reactive form feature in registration form

--- src/app/component/registration/registration.component.ts
+++ src/app/component/registration/registration.component.ts
@@ -1,6 +1,6 @@
 import { CookieService } from 'ngx-cookie-service';
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm,FormBuilder,FormGroup, Validators,FormArray } from '@angular/forms';
 import * as $ from 'jquery';
 import { ServiceService } from 'src/app/services/service.service';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
@@ -15,86 +15,113 @@ import { YourinvovementComponent } from './yourinvovement/yourinvovement.compone
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  show !:string;
-  errorshow !:string;
-  fname !:string;
-  gtype !:string;
-  presentaddress!:string;
-  mno!:string;
-  emailid!:string;
-  img:any;
-  a:any;
-  ref:any;
-  task:any;
-  randomId= Math.random().toString(36).substring(2);
-  yourinvolvement:[];
-
+  // show !:string;
+  // errorshow !:string;
+  // fname !:string;
+  // gtype !:string;
+  // presentaddress!:string;
+  // mno!:string;
+  // emailid!:string;
+  // img:any;
+  // a:any;
+  // ref:any;
+  // task:any;
+  // randomId= Math.random().toString(36).substring(2);
+  // yourinvolvement:[];
+   registrationForm:FormGroup;
+   involvewithBITA:FormGroup;
   constructor(
     private firebaseservice:ServiceService,
     private asstorage:AngularFireStorage,
     private spinner:NgxSpinnerService,
     private dialog: MatDialog,
-    private cookieservice:CookieService
+    private cookieservice:CookieService,
+    private fb:FormBuilder
 
   ) { }
 
   ngOnInit(): void {
-  }
-  upload(event:Event){
-    this.a=(event.target as HTMLInputElement).files[0];
-    let reader=new FileReader();
-    reader.readAsDataURL(this.a);
-    reader.onload=()=>{
-      this.img=reader.result
-    };
-  }
-async onSubmit(form: NgForm){
-  this.spinner.show();
-  const getProjectInvovementdata=JSON.parse(this.cookieservice.get('yourinvolvement'));
-  if(this.a){
-    this.ref = this.asstorage.ref(this.randomId);
-    this.task = this.ref.put(this.a).then((res:any)=>{
-      if(res){
-      const downloadURL=this.ref.getDownloadURL().subscribe((url:any)=>{
-        URL=url;
-        if(URL.length>0){
-          let res={
-            fname :form.value.fname,
-            gtype:form.value. gtype,
-            email:form.value.emailid,
-            address:form.value.presentaddress,
-            mno:form.value.mno,
-            img:URL,
-            userid:this.randomId,
-            involvedata:getProjectInvovementdata
-          }
-          this.firebaseservice.submitnewmember(res).then(e=>{
-                if(e){
-                  this.spinner.hide();
-                  this.dialog.open(SuccessmessageComponent);
-                   form.reset();
-                   this.img=""
-                }else{
-                  this.spinner.hide();
-                }
-              });
-          }
-      })
-      }
-      }).catch((error:any)=>{
-            console.log(error); 
+    this.registrationForm=this.fb.group({
+      fullname:[null,Validators.required,Validators.minLength],
+      gender:[null,Validators.required],
+      address:[null,Validators.required,Validators.minLength],
+      mno:[null,Validators.required,Validators.pattern,Validators.maxLength,Validators.minLength],
+      email:[null,Validators.required,Validators.pattern],
+      involvewithBITA:this.fb.group({
+        tickets:this.fb.array([this.createField()],Validators.required)
+              }) 
+      
     })
-  } else {
-        this.spinner.hide();
-        form.reset();
-        alert("You forget to add your photo.")
-  }
+
 
   }
+  // upload(event:Event){
+  //   this.a=(event.target as HTMLInputElement).files[0];
+  //   let reader=new FileReader();
+  //   reader.readAsDataURL(this.a);
+  //   reader.onload=()=>{
+  //     this.img=reader.result
+  //   };
+  // }
+// async onSubmit(form: NgForm){
+//   this.spinner.show();
+//   const getProjectInvovementdata=JSON.parse(this.cookieservice.get('yourinvolvement'));
+//   if(this.a){
+//     this.ref = this.asstorage.ref(this.randomId);
+//     this.task = this.ref.put(this.a).then((res:any)=>{
+//       if(res){
+//       const downloadURL=this.ref.getDownloadURL().subscribe((url:any)=>{
+//         URL=url;
+//         if(URL.length>0){
+//           let res={
+//             fname :form.value.fname,
+//             gtype:form.value. gtype,
+//             email:form.value.emailid,
+//             address:form.value.presentaddress,
+//             mno:form.value.mno,
+//             img:URL,
+//             userid:this.randomId,
+//             involvedata:getProjectInvovementdata
+//           }
+//           this.firebaseservice.submitnewmember(res).then(e=>{
+//                 if(e){
+//                   this.spinner.hide();
+//                   this.dialog.open(SuccessmessageComponent);
+//                    form.reset();
+//                    this.img=""
+//                 }else{
+//                   this.spinner.hide();
+//                 }
+//               });
+//           }
+//       })
+//       }
+//       }).catch((error:any)=>{
+//             console.log(error); 
+//     })
+//   } else {
+//         this.spinner.hide();
+//         form.reset();
+//         alert("You forget to add your photo.")
+//   }
+
+//   }
   yourinvolvementdetails(){
     this.dialog.open(YourinvovementComponent,{
       width:'70%',
     });
   }
-
+  onSubmit(){ }
+  createField():FormGroup{
+    return this.fb.group({
+      name:[null,Validators.required],
+      age:[null,Validators.required]
+    })
+  }
+  get tickets():FormArray{
+    return <FormArray>this.registrationForm.get('tickets')
+  }
+  deleteValue(i){
+    this.tickets.removeAt(i);
+  }
 }
